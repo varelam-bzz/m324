78ef810ac3c90a434f3df7c536399cc2784251cf
spken <134706997+spken@users.noreply.github.com>
2026-01-19 08:45:57 +0100

    m324 code

---
+++ 07/02_Exercises/run.md
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.projectKey=trafficlight-m324 \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.token=sqp_e2abc07dd26e804a149284ca5a127704e1b76f42
---
+++ docker.sh
@@ -0,0 +1,10 @@
+ssh -i "~/.ssh/mvarela.pem" -L 8081:localhost:8080 "ubuntu@ec2-54-242-1-147.compute-1.amazonaws.com"
+
+# jenkins:
+admin:38766313c5454d4ea69afdff680b56ff
+
+# jenkins3:
+admin:010b74582af54b619a51a72c42efa405
+
+# sonarqube
+Admin:010b74582af54b619a51a72c42efa405
